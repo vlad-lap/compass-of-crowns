2b22a09c80fedba7d76dc68e72e2b09d13c630e0
vlapshov <vlapshov@iponweb.net>
2026-07-08 13:53:17 +0300

    Add missing locations

--- scripts/locations-in-continent.mjs
+++ scripts/locations-in-continent.mjs
@@ -1,10 +1,16 @@
-import { pointInPolygon } from './point-in-polygon.mjs';
+import { pointInPolygon, distanceToPolygon } from './point-in-polygon.mjs';
 import { filterGeodata } from './filter-geodata.mjs';
+
+const MAX_DISTANCE_FROM_COASTLINE = 0.05;
 
 export function locationsInContinent(locations, continents, islands) {
     return filterGeodata(locations, f => {
         const point = f.geometry.coordinates;
         const polygons = [...(continents?.features ?? []), ...(islands?.features ?? [])];
-        return polygons.some(polygon => pointInPolygon(point, polygon.geometry));
+        return polygons.some(
+            polygon =>
+                pointInPolygon(point, polygon.geometry) ||
+                distanceToPolygon(point, polygon.geometry) <= MAX_DISTANCE_FROM_COASTLINE,
+        );
     });
 }
