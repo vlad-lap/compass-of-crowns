c2c14187f3f3cfef02df182e7547bf24a7dce28a
vlapshov <vlad.lapshov@me.com>
2026-07-14 19:03:54 +0300

    Extract SVG icon to the separate component

--- src/app/components/spinner/spinner.component.ts
+++ src/app/components/spinner/spinner.component.ts
@@ -1,10 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { StarIconComponent } from '../star-icon/star-icon.component';
 
 @Component({
     selector: 'cc-spinner',
     changeDetection: ChangeDetectionStrategy.OnPush,
-    imports: [],
-    templateUrl: './spinner.component.svg',
-    styleUrl: './spinner.component.scss',
+    imports: [StarIconComponent],
+    template: '<cc-star-icon [scale]="0.1" [animated]="true" />',
 })
 export class SpinnerComponent {}
---
+++ src/app/components/star-icon/star-icon.component.ts
@@ -0,0 +1,17 @@
+import { ChangeDetectionStrategy, Component, computed, input } from '@angular/core';
+
+@Component({
+    selector: 'cc-star-icon',
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    templateUrl: './star-icon.component.svg',
+    styleUrl: './star-icon.component.scss',
+    host: {
+        '[style.transform]': 'hostTransform()',
+    },
+})
+export class StarIconComponent {
+    scale = input<number>(1);
+    animated = input<boolean>(false);
+
+    protected hostTransform = computed<string>(() => `scale(${this.scale()})`);
+}
